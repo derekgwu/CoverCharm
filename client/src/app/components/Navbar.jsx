81e8b4b33fa3da98f4c9a792a6a90001374463c1
Derek Chen <dchen36@gwmail.gwu.edu>
2024-09-25 18:39:58 -0400

    readded login

--- client/src/app/components/Navbar.jsx
+++ client/src/app/components/Navbar.jsx
@@ -1,7 +1,15 @@
+'use client';
 import React from "react";
 import "../styles/Navbar.css";
 import { PiPaperPlaneTiltThin } from "react-icons/pi";
+import { useRouter } from 'next/navigation';
+
+
 const Navbar = () => {
+    const router = useRouter();
+    const navigateTo = (link) => {
+        router.push(link);
+    }
     return (
         <div className="navbar">
             <div className="logo-div">
@@ -9,7 +17,7 @@ const Navbar = () => {
             </div>
             <div className="links">
                 <p>About</p>
-                <button className="login-btn">Login</button>
+                <button className="login-btn" onClick={navigateTo("/api/auth/login")}>Login</button>
             </div>
         </div>
     )
