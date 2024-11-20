2213cdf630920d4ef09d4bfd934c82bdcebc9d4c
Hashem Abdelati <hashemabdelati@Hashems-MacBook-Pro.local>
2024-11-20 18:47:02 -0500

    Hashem

---
+++ client/src/app/hashem/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function About() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>this is home</p>
+      </div>
+    </main>
+  );
+}
